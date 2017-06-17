9cc21244d778348612957d3f7bc3dd226b7d95e4
ViKapitoshka <kapitonenko.vick@ya.ru>
2017-06-17 12:54:49 +0300

    start

---
+++ js/transition.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("a[href*=#]").on("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top - 54
+		}, 1000);
+		e.preventDefault();
+		return false;
+	});
+});
