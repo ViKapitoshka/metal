00cd140ed1031798eca17d70cc15de4572f3f24c
ViKapitoshka <kapitonenko.vick@ya.ru>
2017-06-17 14:00:41 +0300

    add icons and line footer

--- js/transition.js
+++ js/transition.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("a[href*=#]").on("click", function(e){
+	$("a.scroll").on("click", function(e){
 		var anchor = $(this);
 		$('html, body').stop().animate({
 			scrollTop: $(anchor.attr('href')).offset().top - 54
