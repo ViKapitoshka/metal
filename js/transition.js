2bc2c94a96df7ecb96c0ef395d7feae350dd424a
vikapitoshka <kapitonenko.vick@ya.ru>
2017-11-21 21:21:16 +0300

    edit parners, add about, edit advantages, edit tra

--- js/transition.js
+++ js/transition.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	$("li a").on("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top - 61
+		}, 1000);
+		e.preventDefault();
+		return false;
+	});
 	$("a.scroll").on("click", function(e){
 		var anchor = $(this);
 		$('html, body').stop().animate({
